6b5b89cfa36c19fa2bb8498adbd4a5bf7ef24f49
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-06 21:14:19 +0700

    Not populating role when getOneById account anymore

--- src/api/v1/components/accounts/controllers/getOneById.js
+++ src/api/v1/components/accounts/controllers/getOneById.js
@@ -3,7 +3,11 @@ import Account from '../model';
 const getOneById = async (req, res) => {
   const { id } = req.params;
   try {
-    const account = await Account.findById(id).populate({ path: 'role' });
+    const account = await Account.findById(id);
+    console.log(
+      '🚀 ~ file: getOneById.js ~ line 7 ~ getOneById ~ account',
+      account
+    );
     if (!account) {
       throw new Error('Not found');
     }
