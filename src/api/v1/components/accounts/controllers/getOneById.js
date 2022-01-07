552d2cd6aa8337d162e99c4b53cffae3769ee274
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-07 09:21:33 +0700

    Add populatedRole field of getOneById account

--- src/api/v1/components/accounts/controllers/getOneById.js
+++ src/api/v1/components/accounts/controllers/getOneById.js
@@ -1,17 +1,18 @@
 import Account from '../model';
+import Role from '../../roles/model';
 
 const getOneById = async (req, res) => {
   const { id } = req.params;
   try {
     const account = await Account.findById(id);
-    console.log(
-      '🚀 ~ file: getOneById.js ~ line 7 ~ getOneById ~ account',
-      account
-    );
+    const role = await Role.findById(account.role);
     if (!account) {
       throw new Error('Not found');
     }
-    res.status(200).send(account);
+    res.status(200).send({
+      ...account._doc,
+      populatedRole: role,
+    });
   } catch (error) {
     res.status(400).send({
       message: error.message,
