7b9d059edc3f2c93ff58461de0507ac88c347d23
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-08 22:02:04 +0700

    Using gradeDetailServices to add gradeBoard to response of classroom component's getOneById controller

--- src/api/v1/components/classrooms/controllers/getOneById.js
+++ src/api/v1/components/classrooms/controllers/getOneById.js
@@ -1,14 +1,23 @@
 import Classroom from '../model';
+import gradeDetailServices from '../../grade_detail/services';
 
 const getOneById = async (req, res) => {
   const { id } = req.params;
-  console.log(id);
   try {
     const classroom = await Classroom.findById(id).populate({ path: 'owner' });
+    const rawGradeDetails = await gradeDetailServices.getByClassroomId(id);
+    const gradeBoard = gradeDetailServices.fulfillGradeBoard(
+      classroom,
+      rawGradeDetails
+    );
+
     if (!classroom) {
       throw new Error('Not found');
     }
-    res.status(200).send(classroom);
+    res.status(200).send({
+      ...classroom._doc,
+      gradeBoard,
+    });
   } catch (error) {
     res.status(400).send({
       message: error.message,
--- src/api/v1/components/grade_detail/router.js
+++ src/api/v1/components/grade_detail/router.js
@@ -2,30 +2,35 @@ import express from 'express';
 import authMiddleware from '../../middlewares/auth';
 import gradeDetailMiddlewares from './middlewares';
 import gradeDetailControllers from './controllers';
+import functionalityList from '../functionalities/constants/functionalityList';
 
 const gradeDetailRouter = express.Router();
 
 // ======================== GET ========================
 gradeDetailRouter.get(
   '/:classroomId',
-  authMiddleware,
+  authMiddleware(functionalityList.canReadClassrooms),
   gradeDetailControllers.getByClassroomId
 );
 gradeDetailRouter.get(
   '/csv/:classroomId',
-  authMiddleware,
+  authMiddleware(functionalityList.canReadClassrooms),
   gradeDetailControllers.getCsvDataByClassroomId
 );
 gradeDetailRouter.get(
   '/csv/:classroomId/:gradeId',
-  authMiddleware,
+  authMiddleware(functionalityList.canReadClassrooms),
   gradeDetailControllers.getCsvDataByGradeId
 );
 // ======================== POST ========================
-gradeDetailRouter.post('/', authMiddleware, gradeDetailControllers.postOne);
+gradeDetailRouter.post(
+  '/',
+  authMiddleware(functionalityList.canReadClassrooms),
+  gradeDetailControllers.postOne
+);
 gradeDetailRouter.post(
   '/csv/:classroomId/:gradeId',
-  authMiddleware,
+  authMiddleware(functionalityList.canReadClassrooms),
   gradeDetailMiddlewares.uploadCsvFile,
   gradeDetailControllers.postCsvDataByGradeId
 );
