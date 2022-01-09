3c8a50c8898317d0df64ac56c8d31766862cdc94
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-09 15:42:23 +0700

    Update getOneById service of role - not populating functionalityList anymore

--- src/api/v1/components/roles/services/getOneById.js
+++ src/api/v1/components/roles/services/getOneById.js
@@ -1,9 +1,10 @@
 import Role from '../model';
 
 const getOneById = async (roleId) => {
-  const role = await Role.findById(roleId).populate({
-    path: 'functionalityList',
-  });
+  const role = await Role.findById(roleId);
+  // const role = await Role.findById(roleId).populate({
+  //   path: 'functionalityList',
+  // });
   return role;
 };
 
